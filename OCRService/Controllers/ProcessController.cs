147452ced9f7e561fdddcc98781557d07c858b2f
Matúš Brliť <matus.brlit@gmail.com>
2015-08-03 21:42:43 +0200

    method for processing pdf
    service now extracts image from pdf, code cleanup

--- OCRService/Controllers/ProcessController.cs
+++ OCRService/Controllers/ProcessController.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Hosting;
 using System.Web.Http;
+using OCRService.Utils;
 using Tesseract;
 using System.Drawing;
 using System.IO;
@@ -18,10 +17,10 @@ namespace OCRService.Controllers
         {
             try
             {
-                System.Net.WebClient objWebClient = new System.Net.WebClient();
-                var targetFile = HostingEnvironment.MapPath(@"~/PicturesToProcess/") + Path.GetRandomFileName() + Path.GetExtension(path);
+                var objWebClient = new WebClient();
+                var targetFile = HostingEnvironment.MapPath(@"~/FilesToProcess/") + Path.GetRandomFileName() + Path.GetExtension(path);
                 objWebClient.DownloadFile(path, targetFile);
-                Bitmap picture = new Bitmap(targetFile);
+                var picture = new Bitmap(targetFile);
                 string result = Process(picture);
                 picture.Dispose();
                 File.Delete(targetFile);
@@ -37,9 +36,29 @@ namespace OCRService.Controllers
         {
             try
             {
-                pictureString=pictureString.Replace("data:image/png;base64,","");
-                Bitmap picture = Base64StringToBitmap(pictureString);
+                pictureString = pictureString.Replace("data:image/png;base64,", "");
+                Bitmap picture = ImageHelper.Base64StringToBitmap(pictureString);
                 string result = Process(picture);
+                var resp = new HttpResponseMessage(HttpStatusCode.OK);
+                resp.Content = new StringContent(result);
+                return resp;
+            }
+            catch { throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, new Exception("Error"))); }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Pdf([FromBody]string path)
+        {
+            try
+            {
+                var objWebClient = new WebClient();
+                var targetFile = HostingEnvironment.MapPath(@"~/FilesToProcess/") + Path.GetRandomFileName() + Path.GetExtension(path);
+                objWebClient.DownloadFile(path, targetFile);
+
+                var picture = ImageHelper.ExtractImageFromPdf(targetFile);
+                string result = Process(picture);
+                picture.Dispose();
+                File.Delete(targetFile);
                 var resp = new HttpResponseMessage(HttpStatusCode.OK);
                 resp.Content = new StringContent(result);
                 return resp;
@@ -49,36 +68,16 @@ namespace OCRService.Controllers
 
         private string Process(Bitmap picture)
         {
-            using (var engine = new TesseractEngine(HostingEnvironment.MapPath(@"~/tessdata"), "slk", EngineMode.Default))
+            using (var engine = new TesseractEngine(HostingEnvironment.MapPath(@"~/tessdata"), "slk"))
             {
                 engine.DefaultPageSegMode = PageSegMode.SingleBlock;
-                
-                using (var pix = PixConverter.ToPix(picture))
+
+                using (var page = engine.Process(picture))
                 {
-                    using (var page = engine.Process(pix))
-                    {
-                        return page.GetText();
-                    }
+
+                    return page.GetText();
                 }
             }
-        }
-
-        public Bitmap Base64StringToBitmap(string base64String)
-        {
-            Bitmap bmpReturn = null;
-
-            byte[] byteBuffer = Convert.FromBase64String(base64String);
-            MemoryStream memoryStream = new MemoryStream(byteBuffer);
-
-            memoryStream.Position = 0;
-
-            bmpReturn = (Bitmap)Bitmap.FromStream(memoryStream);
-
-            memoryStream.Close();
-            memoryStream = null;
-            byteBuffer = null;
-
-            return bmpReturn;
         }
     }
 }
---
+++ OCRService/Utils/ImageHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using iTextSharp.text.pdf;
+
+namespace OCRService.Utils
+{
+    public static class ImageHelper
+    {
+        public static Bitmap ExtractImageFromPdf(string sourcePdf)
+        {
+            var pdf = new PdfReader(sourcePdf);
+
+            try
+            {
+                PdfDictionary pg = pdf.GetPageN(1);
+                var res = (PdfDictionary)PdfReader.GetPdfObject(pg.Get(PdfName.RESOURCES));
+                var xobj = (PdfDictionary)PdfReader.GetPdfObject(res.Get(PdfName.XOBJECT));
+
+                foreach (PdfName name in xobj.Keys)
+                {
+                    PdfObject obj = xobj.Get(name);
+                    if (!obj.IsIndirect()) continue;
+                    var tg = (PdfDictionary)PdfReader.GetPdfObject(obj);
+                    var type = (PdfName)PdfReader.GetPdfObject(tg.Get(PdfName.SUBTYPE));
+                    if (!PdfName.IMAGE.Equals(type)) continue;
+                    var xrefIndex = Convert.ToInt32(((PRIndirectReference)obj).Number.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                    var pdfObj = pdf.GetPdfObject(xrefIndex);
+                    var pdfStrem = (PdfStream)pdfObj;
+                    byte[] bytes = PdfReader.GetStreamBytesRaw((PRStream)pdfStrem);
+                    if ((bytes == null)) continue;
+
+                    using (var memStream = new MemoryStream(bytes))
+                    {
+                        memStream.Position = 0;
+                        return new Bitmap(memStream);
+                    }
+                }
+            }
+            finally
+            {
+                pdf.Close();
+            }
+
+            return null;
+        }
+
+        public static ImageCodecInfo GetImageEncoder(string imageType)
+        {
+            imageType = imageType.ToUpperInvariant();
+
+            foreach (ImageCodecInfo info in ImageCodecInfo.GetImageEncoders())
+            {
+                if (info.FormatDescription == imageType)
+                {
+                    return info;
+                }
+            }
+
+            return null;
+        }
+
+        public static Bitmap Base64StringToBitmap(string base64String)
+        {
+            byte[] byteBuffer = Convert.FromBase64String(base64String);
+            var memoryStream = new MemoryStream(byteBuffer);
+
+            memoryStream.Position = 0;
+
+            var bmpReturn = (Bitmap)Image.FromStream(memoryStream);
+
+            memoryStream.Close();
+
+            return bmpReturn;
+        }
+    }
+}
