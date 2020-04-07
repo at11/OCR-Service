79a01ddce8ba60e569e31e89d96ccf255910db77
Ján Neščivera <jan.nescivera@erni.sk>
2020-04-07 14:11:58 +0200

    refactor to process all data in memory, use imagemagick for pdf conversion, updated nugets

--- OCRService/Controllers/ProcessController.cs
+++ OCRService/Controllers/ProcessController.cs
@@ -1,80 +1,68 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Web.Hosting;
 using System.Web.Http;
 using OCRService.Utils;
 using Tesseract;
 using System.Drawing;
-using System.IO;
 
 namespace OCRService.Controllers
 {
     public class ProcessController : ApiController
     {
         [HttpPost]
-        public HttpResponseMessage Url([FromBody]string path)
+        public HttpResponseMessage Image([FromBody] string url)
         {
-            try
+            using (var objWebClient = new WebClient())
             {
-                var objWebClient = new WebClient();
-                var targetFile = HostingEnvironment.MapPath(@"~/FilesToProcess/") + Path.GetRandomFileName() + Path.GetExtension(path);
-                objWebClient.DownloadFile(path, targetFile);
-                var picture = new Bitmap(targetFile);
-                string result = Process(picture);
-                picture.Dispose();
-                File.Delete(targetFile);
-                var resp = new HttpResponseMessage(HttpStatusCode.OK);
-                resp.Content = new StringContent(result);
-                return resp;
+                var bytes = objWebClient.DownloadData(url);
+                using (var picture = ImageHelper.ConvertImage(bytes))
+                {
+                    var result = Process(picture);
+                    return new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent(result)
+                    };
+                }
             }
-            catch { throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, new Exception("Error"))); }
         }
 
         [HttpPost]
-        public HttpResponseMessage Encoded([FromBody]string pictureString)
+        public HttpResponseMessage Encoded([FromBody] string base64)
         {
-            try
+            using (var picture = ImageHelper.ConvertBase64String(base64))
             {
-                pictureString = pictureString.Replace("data:image/png;base64,", "");
-                Bitmap picture = ImageHelper.Base64StringToBitmap(pictureString);
-                string result = Process(picture);
-                var resp = new HttpResponseMessage(HttpStatusCode.OK);
-                resp.Content = new StringContent(result);
-                return resp;
+                var result = Process(picture);
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(result)
+                };
             }
-            catch { throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, new Exception("Error"))); }
         }
 
         [HttpPost]
-        public HttpResponseMessage Pdf([FromBody]string path)
+        public HttpResponseMessage Pdf([FromBody] string url)
         {
-            try
+            using (var objWebClient = new WebClient())
             {
-                var objWebClient = new WebClient();
-                var targetFile = HostingEnvironment.MapPath(@"~/FilesToProcess/") + Path.GetRandomFileName() + Path.GetExtension(path);
-                objWebClient.DownloadFile(path, targetFile);
-
-                var picture = ImageHelper.ExtractImageFromPdf(targetFile);
-                string result = Process(picture);
-                picture.Dispose();
-                File.Delete(targetFile);
-                var resp = new HttpResponseMessage(HttpStatusCode.OK);
-                resp.Content = new StringContent(result);
-                return resp;
+                var bytes = objWebClient.DownloadData(url);
+                using (var picture = ImageHelper.ConvertPdf(bytes))
+                {
+                    var result = Process(picture);
+                    return new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent(result)
+                    };
+                }
             }
-            catch { throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, new Exception("Error"))); }
         }
 
-        private string Process(Bitmap picture)
+        private static string Process(Bitmap picture)
         {
             using (var engine = new TesseractEngine(HostingEnvironment.MapPath(@"~/tessdata"), "slk"))
             {
-                engine.DefaultPageSegMode = PageSegMode.SingleBlock;
-
                 using (var page = engine.Process(picture))
                 {
-
                     return page.GetText();
                 }
             }
--- OCRService/Global.asax.cs
+++ OCRService/Global.asax.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Hosting;
 using System.Web.Http;
-using System.Web.Routing;
+using ImageMagick;
 
 namespace OCRService
 {
@@ -12,6 +9,7 @@ namespace OCRService
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            MagickNET.SetGhostscriptDirectory(HostingEnvironment.MapPath(@"~/ghostscript"));
         }
     }
 }
